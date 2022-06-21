105137f6b8d24d6cbffdac35ccf1bebf914927f6
akril1k <ihorkulinych@gmail.com>
2022-06-21 15:14:05 +0300

    docker

--- api.py
+++ api.py
@@ -19,7 +19,6 @@ db_hostname = os.environ.get('DB_HOSTNAME')
 db_name = os.environ.get('DB_NAME')
  
 DB_URI = 'mysql+pymysql://{db_username}:{db_password}@{db_host}/{database}'.format(db_username=db_user, db_password=db_pass, db_host=db_hostname, database=db_name)
- 
 engine = create_engine(DB_URI, echo=True)
  
 app = Flask(__name__)
